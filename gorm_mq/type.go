53b2056be349c65909c352b3d4da2391f0e5fde1
zwl <1633720889@qq.com>
2023-09-25 23:28:17 +0800

    gorm实现

---
+++ gorm_mq/type.go
@@ -0,0 +1,22 @@
+package gorm_mq
+
+import (
+	"github.com/ecodeclub/mq-sql/gorm_mq/getter/poll"
+)
+
+type ProducerGetter interface {
+	Get(key string) int64
+}
+
+type ConsumerBalancer interface {
+	// Balance 返回的是每个消费者他的分区
+	Balance(partition int, consumers int) [][]int
+}
+
+type NewProducerGetter func(partition int) ProducerGetter
+
+func NewGetter(partition int) ProducerGetter {
+	return &poll.Getter{
+		Partition: partition,
+	}
+}
